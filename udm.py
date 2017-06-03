895890dc246023d7649cf669078881e1dbd7a0ab
Prashant <prashant_chogle@yahoo.com>
2017-06-03 12:56:06 +0000

    code completed

--- udm.py
+++ udm.py
@@ -1,6 +1,23 @@
 import time
 import RPi.GPIO ass GPIO
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(13,GPIO.OUT)
+TRIG = 11
+ECHO = 12
+GPIO.setup(TRIG,GPIO.OUT)
+GPIO.output(TRIG,0)
+GPIO.output(ECHO,GPIO.IN)
+time.sleep(0.1)
+print "Starting measurement..."
+GPIO.output(TRIG,1)
+time.sleep(0.0001)
+GPIO.output(TRIG,0)
+while GPIO.input(ECHO)==0:
+	pass
+start = time.time()
+
+while GPIO.input(ECHO)--1:
+	pass
+stop = time.time()
+print (stop-start)*17000
 GPIO.cleanup()
 
