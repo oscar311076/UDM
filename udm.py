3474afba76baed9a7083b37a36ab0050dd92e956
oscar311076 <prashant_chogle@yahoo.com>
2017-06-16 18:50:18 +0000

    continuous loop added by adding while loop

--- udm.py
+++ udm.py
@@ -3,21 +3,22 @@ import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BOARD)
 TRIG = 11
 ECHO = 12
-GPIO.setup(TRIG,GPIO.OUT)
-GPIO.output(TRIG,0)
-GPIO.setup(ECHO,GPIO.IN)
-time.sleep(0.1)
-print "Starting measurement..."
-GPIO.output(TRIG,1)
-time.sleep(0.0001)
-GPIO.output(TRIG,0)
-while GPIO.input(ECHO)==0:
-	pass
-start = time.time()
+while(1):
+	GPIO.setup(TRIG,GPIO.OUT)
+	GPIO.output(TRIG,0)
+	GPIO.setup(ECHO,GPIO.IN)
+	time.sleep(0.1)
+	#print "Starting measurement..."
+	GPIO.output(TRIG,1)
+	time.sleep(0.0001)
+	GPIO.output(TRIG,0)
+	while GPIO.input(ECHO)==0:
+		pass
+		start = time.time()
 
-while GPIO.input(ECHO)==1:
-	pass
-stop = time.time()
-print (stop-start)*17000
+	while GPIO.input(ECHO)==1:
+		pass
+		stop = time.time()
+	print (stop-start)*17000
 GPIO.cleanup()
 
